b7d2646244dac2ac7baa1d08b96f5329149770fc
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-05-31 18:15:10 +0000

    modifications

--- Day 4/q13_GenFiboSeries.cpp
+++ Day 4/q13_GenFiboSeries.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int fibonacci(int x){
-    if(x<1){
-        return 0;
-    }
-    if(x==1 || x == 2){
-        return 1;
-    }
-    return fibonacci(x-1) + fibonacci(x-2);
+    // if(x<1){
+    //     return 0;
+    // }
+    // if(x==1 || x == 2){
+    //     return 1;
+    // }
+    return (x<=1) ? x: fibonacci(x-1) + fibonacci(x-2);
 }
 
 int main(){
