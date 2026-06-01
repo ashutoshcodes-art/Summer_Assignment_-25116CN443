91a49c020a074d851b3bacb9ec3f74a98ac5260e
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-01 16:13:31 +0000

    changes

--- Day 4/q13_GenFiboSeries.cpp
+++ Day 4/q13_GenFiboSeries.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int x){
-    // if(x<1){
-    //     return 0;
-    // }
-    // if(x==1 || x == 2){
-    //     return 1;
-    // }
-    return (x<=1) ? x: fibonacci(x-1) + fibonacci(x-2);
-}
+// int fibonacci(int x){
+//     // if(x<1){
+//     //     return 0;
+//     // }
+//     // if(x==1 || x == 2){
+//     //     return 1;
+//     // }
+//     return (x<=1) ? x: fibonacci(x-1) + fibonacci(x-2);
+// }
 
 int main(){
     int n;
     cout<<"Enter how many terms of Fibonacci You wanna Print : ";
     cin>>n;
     cout << "Fib0nacci series upto "<<n<<" terms is : "<<endl;
-    for(int i = 0 ; i<=n ; i++){
-        cout<<fibonacci(i)<<" ";
+    int a=0,b=1,NextTerm;
+    for (int k = 1; k <= n ; k++){
+        cout << a<<" ";
+                NextTerm = a + b;
+                a = b;
+                b = NextTerm;
     }
+
+    // for(int i = 0 ; i<=n ; i++){
+    //     cout<<fibonacci(i)<<" ";
+    // }
     return 0;
 }
--- Day 4/q14_NthTermFibo.cpp
+++ Day 4/q14_NthTermFibo.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(int x){
-    // if(x<1){
-    //     return 0;
-    // }
-    // if(x==1 || x == 2){
-    //     return 1;
-    // }
-    return (x<=1) ? x :  fibonacci(x-1) + fibonacci(x-2);
-}
+// int fibonacci(int x){
+//     // if(x<1){
+//     //     return 0;
+//     // }
+//     // if(x==1 || x == 2){
+//     //     return 1;
+//     // }
+//     return (x<=1) ? x :  fibonacci(x-1) + fibonacci(x-2);
+// }
 
 int main(){
     int n;
     cout<<"Enter which term of Fibonacci You wanna Print : ";
     cin>>n;
-    (n<0) ? (cout<<"Enter number >0") : (cout<<n<<"th term of fibonacci series is : "<<fibonacci(n));
+    int a=0,b=1,NextTerm;
+    if(n==1){
+        cout<<"0";
+    }
+    else{
+         for (int i = 1; i < n; i++)
+    {
+        NextTerm = a+b; a = b; b= NextTerm; 
+    }
+    cout<<a;
+    
+    }
+   
+    // (n<0) ? (cout<<"Enter number >0") : (cout<<n<<"th term of fibonacci series is : "<<fibonacci(n));
     return 0;
 }
