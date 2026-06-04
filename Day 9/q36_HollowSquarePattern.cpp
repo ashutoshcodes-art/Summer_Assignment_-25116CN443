02bb5afdf676f5530a7ba66a7e000915e5a3fbb8
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-04 08:40:48 +0000

    -

--- Day 9/q36_HollowSquarePattern.cpp
+++ Day 9/q36_HollowSquarePattern.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int n; 
+    cout<<"Enter no of rows : ";cin>>n;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n  ; col++)
+        {
+            if(col ==1 || col == n || row == 1 || row== n){
+                cout<<"*";
+            }
+            else cout<< " ";
+        }
+        cout<<endl;
+    }
+        
     return 0;
 }
