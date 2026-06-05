cb5ed9b1bb4989f33d785ede74f73784f63b1b30
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-05 22:12:19 +0530

    Create q54_FreqofEle.cpp

---
+++ Day 14/q54_FreqofEle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of terms yo want in array : ";cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele no. "<<i+1<<" : ";cin>>arr[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"]";
+    int find,count=0;
+    cout<<"Enter number to find freq. in this String : ";cin>>find;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]==find){
+            count++;
+        }
+    }
+    cout<<"Frequency of "<<find<<" is : "<<count;
+    return 0;
+}
