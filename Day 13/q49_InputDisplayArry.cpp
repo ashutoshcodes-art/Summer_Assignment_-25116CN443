56ca6b20dd3d038147c3a9c2fa4e340a12116fa1
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-05 04:17:10 +0000

    -

---
+++ Day 13/q49_InputDisplayArry.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of terms yo want in array : ";cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele no. "<<i+1<<" : ";cin>>arr[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<" ]";
+    
+    return 0;
+}
