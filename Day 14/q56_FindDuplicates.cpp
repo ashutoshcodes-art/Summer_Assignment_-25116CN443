89eea2c316a353f90cf8eab902b164b16dba1e75
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-06 16:58:20 +0000

    -

---
+++ Day 14/q56_FindDuplicates.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;cout<<"Enter number of elements in array : ";cin>>n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele "<<i+1<<" : ";cin>>a[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        bool visited = false;
+
+        for (int j = 0; j < i; j++)
+        {
+            if(a[j]==a[i]){
+                visited = true;
+            }
+        }
+
+        if(visited){
+            continue;
+        }
+        else { int count=0;
+            for (int k = 0; k < n; k++)
+            {
+                if(a[i]==a[k]){
+                    count++;
+                }
+            }
+            if(count>1) {cout<<a[i]<<" found "<<count<<" times"<<endl;}
+        }
+        
+    }
+    
+    return 0;
+}
