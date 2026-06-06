2c857acb0f042bdb43cc247fa2dc8ea1d3d329e5
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-06 19:10:43 +0000

    -

---
+++ Day 16/q63_PairwithGivenSUm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;cout<<"Enter number of elements in array : ";cin>>n;if(n<=0){return 0;}
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele "<<i+1<<" : ";cin>>a[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+
+    //Checking frequencies 
+    int sum;cout<<"Enter sum : ";cin>>sum;
+    int pair=0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n-1; j++)
+        {
+            if(a[i]+a[j]==sum){
+                cout<<a[i]<<" + "<<a[j]<<" = "<<sum<<" : ("<<a[i]<<","<<a[j]<<")"<<endl;
+                pair++;
+            }
+        }
+        
+    }
+    if(pair==0){
+        cout<<"No Pair found.";
+    }
+    return 0;
+}
