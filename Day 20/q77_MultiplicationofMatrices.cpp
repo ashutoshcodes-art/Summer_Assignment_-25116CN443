d9d25ce6c976d46934f08ca0e95da2304443c828
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-09 16:15:50 +0000

    -

--- Day 20/q77_MultiplicationofMatrices.cpp
+++ Day 20/q77_MultiplicationofMatrices.cpp
@@ -2,23 +2,24 @@
 #include <vector>
 using namespace std;
 int main(){
-    int a,b;cout<<"Enter the dimentions of Matrices in order (m X n): ";cin>>a>>b;
+    int row1,col1;cout<<"Enter the dimentions of !st Matrices in order (m X n): ";cin>>row1>>col1;
+    int row2,col2;cout<<"Enter the dimentions of !st Matrices in order (m X n): ";cin>>row2>>col2;
     //Input Matrices
     cout<<"Taking Input of first matrix : "<<endl;
-    vector<vector<int>> m1(a,vector<int> (b));
-    for (int i = 0; i < a; i++)
+    vector<vector<int>> m1(row1,vector<int> (col1));
+    for (int i = 0; i < row1; i++)
     {
-        for (int j = 0; j < b; j++)
+        for (int j = 0; j < col1; j++)
         {
             cout<<"Enter term ("<<i<<","<<j<<") : ";cin>>m1[i][j];
         }
     }
 
     cout<<"Taking Input of second matrix : "<<endl;
-    vector<vector<int>> m2(b,vector<int> (a));
-    for (int i = 0; i < b; i++)
+    vector<vector<int>> m2(row2,vector<int> (col2));
+    for (int i = 0; i < row2; i++)
     {
-        for (int j = 0; j < a; j++)
+        for (int j = 0; j < col2; j++)
         {
             cout<<"Enter term ("<<i<<","<<j<<") : ";cin>>m2[i][j];
         }
@@ -26,18 +27,18 @@ int main(){
 
     //Printing Input Matrix
     cout<<"Your Entered first Matrix is : "<<endl;
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i < row1; i++)
     {
-        for (int j = 0; j < b; j++)
+        for (int j = 0; j < col1; j++)
         {
             cout<<m1[i][j]<<" ";
         }
         cout<<endl;
     }
     cout<<"Your Entered Second Matrix is : "<<endl;
-    for (int i = 0; i < b; i++)
+    for (int i = 0; i < row2; i++)
     {
-        for (int j = 0; j < a; j++)
+        for (int j = 0; j < col2; j++)
         {
             cout<<m2[i][j]<<" ";
         }
@@ -45,12 +46,16 @@ int main(){
     }
     
     //Multiplication of two martrices
-    vector<vector <int>> mult(a,vector<int> (a));
-    for(int i =0 ;i<a ;i++){
-        for (int j = 0; j < a; j++)
+    if(col1!=row2){
+        cout<<"Multiplication Not defined ";
+        return 0;
+    }
+    vector<vector <int>> mult(row1,vector<int> (col2));
+    for(int i =0 ;i<row1 ;i++){
+        for (int j = 0; j < col2; j++)
         {
             mult[i][j]=0;
-            for (int k = 0; k < b; k++)
+            for (int k = 0; k < col1; k++)
             {
                 mult[i][j] += m1[i][k]*m2[k][j];
             }
@@ -62,9 +67,9 @@ int main(){
 
     //Printing Multiplication result
     cout<<"Multiplication of 1st & 2nd Matrix is : "<<endl;
-    for (int i = 0; i < a; i++)
+    for (int i = 0; i < row1; i++)
     {
-        for (int j = 0; j < a; j++)
+        for (int j = 0; j < col2; j++)
         {
             cout<<mult[i][j]<<" ";
         }
