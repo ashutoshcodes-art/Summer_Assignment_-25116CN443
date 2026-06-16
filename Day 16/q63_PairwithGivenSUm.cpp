70eeb494168bd468e51237820b00360d5dec195c
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-16 11:06:00 +0530

    -

---
+++ Day 16/q63_PairwithGivenSUm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;cout<<"Enter number of elements in array : ";cin>>n;if(n<=0){return 0;}
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele "<<i+1<<" : ";cin>>a[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+
+    //Input Sun & Checking for pairs
+    int sum;cout<<"Enter sum : ";cin>>sum;
+    int pair=0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n-1; j++)
+        {
+            if(a[i]+a[j]==sum){
+                cout<<a[i]<<" + "<<a[j]<<" = "<<sum<<" : ("<<a[i]<<","<<a[j]<<")"<<endl;
+                pair++;
+            }
+        }
+        
+    }
+    if(pair==0){
+        cout<<"No Pair found.";
+    }
+    return 0;
+}
---
+++ Day 16/q64_RemoveDuplicatesArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    //Input & Display Array
+    int n;cout<<"Enter number of elements in array : ";cin>>n;if(n<=0){return 0;}
+    vector<int> a(n); //Uses Heap memory or use pointer //int*a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele "<<i+1<<" : ";cin>>a[i];
+    }
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+
+    //Removing Duplicates
+    int term=0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if(a[i]==a[j]){
+                for (int k = j; k < n-1; k++)
+                {
+                    a[k] = a[k+1];term++;
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+     //Result Array
+   cout<<"Resultant array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+    return 0;
+}
