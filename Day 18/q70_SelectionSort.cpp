96aa2f89c951c9ebc958499bc266c51b28c258a1
ashutoshcodes-art <kayamatashu1605@gmail.com>
2026-06-18 08:32:40 +0530

    -

---
+++ Day 18/q70_SelectionSort.cpp
@@ -0,0 +1,49 @@
+//It is same as finding intersection
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    //Input & Display Array
+    int n;cout<<"Enter number of elements in array : ";cin>>n;if(n<=0){return 0;}
+    vector<int> a(n); //Uses Heap memory or use pointer //int*a = new int[n];
+    //Input arrays
+    cout<<"Taking array input : "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Enter ele "<<i+1<<" : ";cin>>a[i];
+    }
+    //Printing input arrays
+    cout<<"Your entered array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+
+    //Selection Sort/////
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        int minIndex=i;
+        for (int j = i+1; j < n; j++)
+        {
+            if(a[j]<a[minIndex]){
+                minIndex=j;
+            }
+        }
+        int temp = a[i];
+        a[i]=a[minIndex];
+        a[minIndex]=temp;
+    }
+    
+    
+     //Result Array
+   cout<<"Sorted array is : [ ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<" ]"<<endl;
+    return 0;
+}
